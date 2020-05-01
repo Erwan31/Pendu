c61315540482b86f02ebc4d3b4227f113a87e54d
Erwan31 <spilmonterwan@gmail.com>
2020-05-01 17:06:28 +0100

    Tiny CSS change

--- src/clavier.js
+++ src/clavier.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types'
+import './clavier.css'
 
-const Clavier = ({keyLetter, index, onClick}) => (<div className={`keyletter ${index}`} onClick={() => onClick(keyLetter)}>{keyLetter}</div>)
+const Clavier = ({keyLetter, index, onClick}) => (<div className={"keyletter"} onClick={() => onClick(keyLetter)}>{keyLetter}</div>)
 
 export default Clavier;
--- src/letter.js
+++ src/letter.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types'
+import './letter.css';
 
-const Letter = ({letter, index, feedback}) => (<div className={`letter ${letter}`}>{feedback === 'discovered' ? letter : '_'}</div>)
+const Letter = ({letter, index, feedback}) => (<div className={"key"}>{feedback === 'discovered' ? letter : '_'}</div>)
 
 
 Letter.propTypes = {
